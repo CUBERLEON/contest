945cf52efeee1f7f4bd4afe207debe1e7bf498cf
cuberleon <liubomyr.piadyk@gmail.com>
2018-08-26 20:46:50 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <string>
+#include <algorithm>
+#include <locale>
+#include <vector>
+#include <memory>
+#include <cassert>
+#include <queue>
+#include <functional>
+
+#define ll long long
+
+using namespace std;
+
+constexpr int MAX = 1010;
+constexpr int MOD = 1000000007;
+
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    
+
+#ifdef _DEBUG
+	system("pause");
+#endif
+
+    return 0;
+}
